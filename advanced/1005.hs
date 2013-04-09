64b12fc7489c452e32047f020a11fb5dd7b5e3ef
Kahaani <czhtd@126.com>
2013-04-09 12:52:10 +0800

    a5

---
+++ advanced/1005.hs
@@ -0,0 +1,17 @@
+import Data.List(intercalate)
+import Data.Char(digitToInt)
+
+main = getLine >>= (putStr.func)
+
+func = intercalate " " . map english . show . sum . map digitToInt
+
+english '0' = "zero"
+english '1' = "one"
+english '2' = "two"
+english '3' = "three"
+english '4' = "four"
+english '5' = "five"
+english '6' = "six"
+english '7' = "seven"
+english '8' = "eight"
+english '9' = "nine"
